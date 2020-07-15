cfac6c605927f07fd9c6d099691195e55e73e029
vospader13 <justenmcdonald@gmail.com>
2020-07-14 18:03:13 -0600

    Project functional

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,26 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if(item.enhancement === 20) {
+    return {...item}
+  } else {
+    return {...item, enhancement: item.enhancement + 1}
+  }
 }
+
 
 function fail(item) {
-  return { ...item };
-}
+  if(item.enhancement < 15) {
+    return {...item, durability: item.durability - 5}
+  } else if(item.enhancement >= 15) {
+    return {...item, durability: item.durability - 10}
+  } else if(item.enhancement > 16) {
+    return {...item, enhancement: item.enhancement - 1}
+  }
+  }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,35 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+const item = {
+    name: "Justen McDonald",
+    durability: 100,
+    enhancement: 20
+}
+
+describe("testing enchancer.js", () => {
+    test("repair", () => {
+        expect(enhancer.repair(item).durability).toEqual(100)
+    })
+
+    test("succeed", () => {
+        const newItem = enhancer.succeed(item)
+        const defEnhancement = item.enhancement
+
+        if(item.enhancement === 20) {
+            expect(newItem.enhancement).toEqual(defEnhancement)
+        } else {
+            expect(newItem.enhancement).toEqual(defEnhancement + 1)
+        }
+    })
+
+    test("fail", () => {
+        if(item.enhancement < 15) {
+            expect(enhancer.fail(item).durability).toEqual(item.durability - 5)
+        } else if(item.enhancement >= 15) {
+            expect(enhancer.fail(item).durability).toEqual( item.durability - 10)
+        } else if(item.enhancement > 16){
+            expect(enhancer.fail(item).enhancement).toEqual( item.enhancement - 1)
+        }
+    })
+})
